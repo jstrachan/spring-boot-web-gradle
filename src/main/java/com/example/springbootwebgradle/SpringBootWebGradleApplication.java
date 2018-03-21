91a218060c300f15dc6b5067f181e05d9fcd99c5
James Strachan <james.strachan@gmail.com>
2018-03-21 15:51:57 +0000

    Initial import

---
+++ src/main/java/com/example/springbootwebgradle/SpringBootWebGradleApplication.java
@@ -0,0 +1,12 @@
+package com.example.springbootwebgradle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootWebGradleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootWebGradleApplication.class, args);
+	}
+}
---
+++ src/test/java/com/example/springbootwebgradle/SpringBootWebGradleApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.springbootwebgradle;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringBootWebGradleApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
